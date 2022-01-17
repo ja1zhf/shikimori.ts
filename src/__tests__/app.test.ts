7bb9a3fa35b019f0ea1c0472d16c431ea08dd320
Ja1z666 <thatsameuser@icloud.com>
2022-01-17 02:19:09 +0200

    1.0.1

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,5 +1,5 @@
 import { Shikimori } from '../shikimori';
 
 test('My Greeter', async () => {
-    expect((await Shikimori.getUser({name : "!ja1z!ja1z!ja1z!ja1z"})).nickname).toBe("!ja1z!ja1z!ja1z!ja1z");
+  expect((await Shikimori.getUser({ name: '!ja1z!ja1z!ja1z!ja1z' })).nickname).toBe('!ja1z!ja1z!ja1z!ja1z');
 });
